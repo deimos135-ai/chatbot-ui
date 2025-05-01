164856a8fea2ea726c222c1fe90c12e417f5d3aa
deimos135-ai <ubnt5000@gmail.com>
2025-05-01 11:05:49 +0300

    Update layout.tsx
    selectedWorkspace?.image_path»

--- app/[locale]/[workspaceid]/layout.tsx
+++ app/[locale]/[workspaceid]/layout.tsx
@@ -58,17 +58,18 @@ export default function WorkspaceLayout({ children }: WorkspaceLayoutProps) {
 
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    ;(async () => {
-      const session = (await supabase.auth.getSession()).data.session
+ useEffect(() => {
+  ;(async () => {
+    const session = (await supabase.auth.getSession()).data.session
 
-      if (!session) {
-        return router.push("/login")
-      } else {
-        await fetchWorkspaceData(workspaceId)
-      }
-    })()
-  }, [])
+    if (!session) {
+      return router.push("/login")
+    } else {
+      await fetchWorkspaceData(workspaceId)
+    }
+  })()
+}, [workspaceId])
+
 
   useEffect(() => {
     ;(async () => await fetchWorkspaceData(workspaceId))()
